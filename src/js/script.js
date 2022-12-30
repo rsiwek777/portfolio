c92af430c7a3c38305ca4abf94348c672d7376cc
unknown <insired@gmail.com>
2022-12-30 05:31:25 +0100

    amendments

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,22 @@
 const burgerBtn = document.querySelector('.burger-btn')
 const nav = document.querySelector('.nav')
 const navItems = document.querySelectorAll('.nav__item')
-const text = document.querySelector('.sec-text')
+const text = document.querySelector('.tooltiptext')
+
+const copyContent = async () => {
+	try {
+		await navigator.clipboard.writeText(text.innerHTML)
+		console.log('Content copied to clipboard!')
+	} catch (err) {
+		console.error('Failed to copy: ', err)
+	}
+	text.style.backgroundColor = 'rgba(140, 255, 0, 0.5)'
+	text.textContent = 'Copied: kozackikozak#0103'
+	setTimeout(() => {
+		text.style.backgroundColor = ''
+		text.textContent = 'kozackikozak#0103'
+	}, 1500)
+}
 
 burgerBtn.addEventListener('click', () => {
 	nav.classList.toggle('nav-active')
